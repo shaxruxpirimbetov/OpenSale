6e77e35d8c86e1731fca23f3df3d73f8c8c11d44
shaxweb <igrayunagitare@gmail.com>
2025-09-15 03:27:00 +0000

    OpenSale

--- OpenSale/settings.py
+++ OpenSale/settings.py
@@ -124,8 +124,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_ROOT = "/data/data/com.termux/files/home/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = "/data/data/com.termux/files/home/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
